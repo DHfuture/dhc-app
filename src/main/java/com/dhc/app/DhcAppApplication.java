c1b3e274160926b4d2cc16f9d43837eb0d18c182
dhc <1668860048@qq.com>
2022-07-28 21:15:21 +0800

    first

---
+++ src/main/java/com/dhc/app/DhcAppApplication.java
@@ -0,0 +1,13 @@
+package com.dhc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhcAppApplication.class, args);
+	}
+
+}
